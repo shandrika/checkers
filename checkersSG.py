53ba37cacf40efa21e513d5a502d05cab534e995
csPython <cspython@AD25TF04NGF1J-64.local>
2019-03-11 10:33:31 -0400

    Singular Square in Window

--- checkersSG.py
+++ checkersSG.py
@@ -6,3 +6,7 @@ sqSz = 60
 #Create Window
 window = GraphWin("Checkers",sqSz*10,sqSz*10)
 window.setCoords(0,0,sqSz*10,sqSz*10)
+
+#Create Square
+sQ = Rectangle (Point(sqSz, sqSz),Point(sqSz*2, sqSz*2))
+sQ.draw (window)
