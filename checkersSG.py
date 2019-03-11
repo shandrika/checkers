6b8b010875bb90ddbca15874df3b46c332d9ef7d
csPython <cspython@AD25TF04NGF1J-64.local>
2019-03-11 10:53:20 -0400

    Create Square With Custom Function

--- checkersSG.py
+++ checkersSG.py
@@ -9,6 +9,7 @@ window.setCoords(0,0,sqSz*10,sqSz*10)
 
 #Create Square Function
 def run_square(row,column):
-    sQ = Rectangle (Point(sqSz*(row+1),sqSz*(column+1)),Point(sqSz*(row+1), sqSz*(column +2)))                    
+    sQ = Rectangle (Point(sqSz*(row+1),sqSz*(column+1)),Point(sqSz*(row+2), sqSz*(column+2)))                    
     sQ.draw (window)
 
+run_square (0,1)
