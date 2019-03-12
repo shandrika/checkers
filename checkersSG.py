01a72434306d3597d463686f6b4be9f3c6ccf18e
csPython <cspython@AD25TF04NGF1J-64.local>
2019-03-12 11:55:52 -0400

    Color Added, But Checker's Pattern Has Not Yet Been Accomplishedls

--- checkersSG.py
+++ checkersSG.py
@@ -7,11 +7,22 @@ sqSz = 60
 window = GraphWin("Checkers",sqSz*10,sqSz*10)
 window.setCoords(0,0,sqSz*10,sqSz*10)
 
+
 #Create Square Function
-def run_square(row,column):
-    sQ = Rectangle (Point(sqSz*(row+1),sqSz*(column+1)),Point(sqSz*(row+2), sqSz*(column+2)))                    
+def run_square(row,column,color):
+    sQ = Rectangle (Point(sqSz*(row+1),sqSz*(column+1)),Point(sqSz*(row+2), sqSz*(column+2)))
+    sQ.setFill(color)
     sQ.draw (window)
 
+sqC = "red" 
 for c in range (8):
     for i in range (8):
-        run_square(i,c)
+        if c * i % 2 == 0:
+            sqC = "black"
+        else :
+            sqC = "red"
+        
+        run_square(i,c,sqC)
+
+
+
