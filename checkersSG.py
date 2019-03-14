bb32c51172dde41ebd5a037cd786c1ec713be127
csPython <cspython@AD25TF04NGF1J-64.local>
2019-03-14 15:39:57 -0400

    Checkerboard's Orientation Complete

--- checkersSG.py
+++ checkersSG.py
@@ -13,16 +13,15 @@ def run_square(row,column,color):
     sQ = Rectangle (Point(sqSz*(row+1),sqSz*(column+1)),Point(sqSz*(row+2), sqSz*(column+2)))
     sQ.setFill(color)
     sQ.draw (window)
-
+    
 sqC = "red" 
 for c in range (8):
     for i in range (8):
-        if c * i % 2 == 0:
-            sqC = "black"
-        else :
+        if (c+1 * i) % 2 == 0:
             sqC = "red"
+        else :
+            sqC = "black"
         
         run_square(i,c,sqC)
-
 
 
